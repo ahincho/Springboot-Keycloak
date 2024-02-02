435ee4686b60a3d2abecbd11fb6919ed9a8c53bf
ahincho <ahincho@unsa.edu.pe>
2024-02-01 21:24:09 -0500

    Remove unused imports

--- src/main/java/com/devstream/learning/auth/application/services/KeycloakService.java
+++ src/main/java/com/devstream/learning/auth/application/services/KeycloakService.java
@@ -13,8 +13,6 @@ import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
